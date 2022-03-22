95be69a16804f966f63f29e7ec3d6c91991cffc9
sanaandmomo <qajjang46@naver.com>
2022-03-23 02:04:24 +0900

    refactor: 저장된 비디오 id로 테스트

--- src/test/app.spec.js
+++ src/test/app.spec.js
@@ -11,6 +11,19 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
     cy.get(SELECTOR.SEARCH_FORM).submit();
   };
 
+  const testWhenSaveVideoIdCorrectly = (testFn) => {
+    submitKeywordCorrectly();
+    cy.get('.video-item__save-button')
+      .first()
+      .invoke('data', 'videoId')
+      .then((videoId) => {
+        cy.get('.video-item__save-button').first().click();
+        cy.get(SELECTOR.MODAL_BACKGROUND).click({ force: true });
+
+        testFn(videoId);
+      });
+  };
+
   beforeEach(() => {
     cy.visit(baseURL);
   });
@@ -41,37 +54,45 @@ describe('구현 결과가 요구사항과 일치해야 한다.', () => {
   });
 
   it('모달에서 영상을 저장하면, 볼 영상 목록을 업데이트 할 수 있어야 한다.', () => {
-    submitKeywordCorrectly();
-    cy.get('.video-item__save-button').first().click();
-    cy.get(SELECTOR.MODAL_BACKGROUND).click({ force: true });
+    const testFn = (videoId) => {
+      cy.get(`${SELECTOR.UNSEEN_VIDEOS} .video-item__check-button`)
+        .first()
+        .invoke('data', 'videoId')
+        .then((unseenVideoId) => {
+          expect(videoId).to.equal(unseenVideoId);
+        });
+    };
 
-    cy.get(SELECTOR.UNSEEN_VIDEOS).children().should('have.length', 1);
+    testWhenSaveVideoIdCorrectly(testFn);
   });
 
   it('볼 비디오의 체크 버튼을 누르면, 본 비디오 탭으로 이동해야 한다.', () => {
-    submitKeywordCorrectly();
-    cy.get('.video-item__save-button').first().click();
-    cy.get(SELECTOR.MODAL_BACKGROUND).click({ force: true });
-    cy.get(`${SELECTOR.UNSEEN_VIDEOS} .video-item__check-button`).first().click();
+    const testFn = (videoId) => {
+      cy.get(`${SELECTOR.UNSEEN_VIDEOS} .video-item__check-button`).first().click();
+      cy.get(`${SELECTOR.SWITCH_BUTTONS} .switch-button[data-tab='seen']`).click();
 
-    cy.get(SELECTOR.SEEN_BUTTON)
-      .click()
-      .then(() => {
-        cy.get(SELECTOR.SEEN_VIDEOS).children().should('have.length', 1);
-      });
+      cy.get(`${SELECTOR.SEEN_VIDEOS} .video-item__delete-button`)
+        .first()
+        .invoke('data', 'videoId')
+        .then((seenVideoId) => {
+          expect(videoId).to.equal(seenVideoId);
+        });
+    };
+
+    testWhenSaveVideoIdCorrectly(testFn);
   });
 
   it('비디오 삭제 버튼을 누르면, 확인 메세지를 확인하고 예 버튼을 누르면 비어있다는 문구를 보여줘야 한다.', () => {
-    submitKeywordCorrectly();
-    cy.get('.video-item__save-button').first().click();
-    cy.get(SELECTOR.MODAL_BACKGROUND).click({ force: true });
+    const testFn = (videoId) => {
+      cy.get(`.video-item__delete-button[data-video-id='${videoId}']`)
+        .first()
+        .click()
+        .then(() => {
+          cy.on('window:confirm', () => true);
+          cy.get(`.video-item__delete-button[data-video-id='${videoId}']`).should('not.be.visible');
+        });
+    };
 
-    cy.get(`${SELECTOR.UNSEEN_VIDEOS} .video-item__delete-button`)
-      .first()
-      .click()
-      .then(() => {
-        cy.on('window:confirm', () => true);
-        cy.get(SELECTOR.UNSEEN_EMPTY_SCREEN).should('be.visible');
-      });
+    testWhenSaveVideoIdCorrectly(testFn);
   });
 });
